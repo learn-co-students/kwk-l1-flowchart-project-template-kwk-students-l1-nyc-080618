1bb8bf6961652f771b2826d8db19021ac5beff04
courtneyk14 <cbrookelley14@gmail.com>
2018-08-17 16:16:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,19 +15,6 @@ class ApplicationController < Sinatra::Base
     erb :results
   end
   
-# =======
-  #   puts "/"
-  #   erb :layout
-  # end
-  
-  # post '/result' do
-    # puts @params
-    # @categories = @params[:category]
-    # puts @params[:category]
-  #   erb :results
-  # end
-  
-# >>>>>>> ae1917fa019fe183e8b3a77b856404fb8c81f81e
 
 end
 
