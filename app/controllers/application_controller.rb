12c9d2150d09a5eab4129187d54d364b1ef6801b
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-17 15:36:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+  
+  get '/about' do
+    erb :about
+  end
+  
+  get '/action' do 
+    erb :action
+  end
+  
+  get '/inspiration' do 
+    erb :inspiration
+  end
+  
+  get '/resources' do 
+    erb :resources
+  end
+  
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
